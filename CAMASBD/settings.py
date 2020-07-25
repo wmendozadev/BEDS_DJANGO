ddae36ec601ed929e0079dee8c55229a78020e21
Winston <wjmendozag87@gmail.com>
2020-07-25 15:18:55 -0600

    protegiendo cambios appTipoCompra que es un Catalogo

--- CAMASBD/settings.py
+++ CAMASBD/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.EstadoVenta',
+    'apps.EstadoCompra',
+    'apps.TipoCompra',
 ]
 
 MIDDLEWARE = [
---
+++ apps/TipoCompra/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from apps.TipoCompra.models import TipoCompra
+# Register your models here.
+site.admin.register(TipoCompra)
